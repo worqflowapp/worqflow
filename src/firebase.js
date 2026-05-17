7ee244232541dd565743bf8fe48130cf01d85191
Adnan Qamar <adnanqamar@Adnans-MacBook-Pro.local>
2026-05-16 23:25:24 -0400

    Add ShopFlowTracker UI

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrcI-X7DKWT5klMJf-yw4srTqFCW6WWkQ",
+  apiKey: "AIzaSyDhh-lq9b8AtN7BEu8cDDXoGI_FlrKvRxg",
   authDomain: "worqflow-67bff.firebaseapp.com",
   projectId: "worqflow-67bff",
   storageBucket: "worqflow-67bff.firebasestorage.app",
   messagingSenderId: "496653312164",
-  appId: "1:496653312164:web:04e79c0a508745fc29c631",
-  measurementId: "G-0G1RW4RJN6"
+  appId: "1:496653312164:web:c856ee685431a9cf29c631",
+  measurementId: "G-KZG8P21LPM"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
